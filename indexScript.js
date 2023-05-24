c7d5a5ecf3a85243311fd22a959d6a523441f1e0
Braden Hollis <113390727+bhollis6@users.noreply.github.com>
2023-05-23 22:34:04 -0600

    Add files via upload

---
+++ indexScript.js
@@ -0,0 +1,10 @@
+const beginButton = document.querySelector('.button-82-pushable');
+
+function button(){
+  beginButton.addEventListener('click', function(){
+    window.location.href = 'round.html';
+  })
+}
+
+button();
+
